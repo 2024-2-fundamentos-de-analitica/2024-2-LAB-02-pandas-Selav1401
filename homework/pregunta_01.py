1c622a6120565bef9ab5f7171af6007818c29a8f
Selav1401 <slavergne@unal.edu.co>
2025-01-07 14:21:41 -0500

    Lab2 completado

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,6 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_01():
     """
@@ -14,3 +15,9 @@ def pregunta_01():
     40
 
     """
+
+    tbl0 = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+    rows = tbl0.shape[0]   
+    return rows
+
+print(pregunta_01())
--- homework/pregunta_06.py
+++ homework/pregunta_06.py
@@ -5,6 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_06():
     """
@@ -15,3 +16,9 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
+
+    tbl0 = pd.read_csv("files/input/tbl1.tsv", sep="\t")
+    tbl0["c4"]
+    return sorted(tbl0["c4"].str.upper().unique())
+
+print(pregunta_06())
